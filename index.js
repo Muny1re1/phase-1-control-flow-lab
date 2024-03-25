72f2b4527c4bb14103438c515533c485f46464c3
Muny1re1 <munyiriandy@gmail.com>
2024-03-25 12:07:37 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let rideDistance;
+function scuberGreetingForFeet(rideDistance){
+  if(rideDistance <= 400){
+   return 'This one is on me!';
+  } else if (rideDistance> 400  && rideDistance <= 2000){
+    return 'That will be twenty bucks.'
+  } else if (rideDistance > 2000 && rideDistance<= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+let city;
+function ternaryCheckCity(city){
+  if(city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else{
+    return 'No go.'
+  }
 }
-
+let tipGenerosity;
 function switchOnCharmFromTip(){
-  // Write your code here!
+if(tipGenerosity === 'Generous'){
+  return 'Thank you very much.'
+} else if (tipGenerosity === 'Not as generous'){
+  return 'Thank you.'
+} else {
+  return 'Bye'
 }
+}
+  
